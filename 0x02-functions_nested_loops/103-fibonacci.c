5a90a9eb477b35c94ac231c77e8d73b634e93b4b
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-09-12 21:55:57 -0700

    Upfate

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,52 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
+int sum(int n);
+int fib(int n);
 
-/**
-  * main - the main printing funtion
-  * less than 4000000.
-  * Return: 0.
-  */
+const int N = 4000000;
 
-
-int main(void)
+int main(void);
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+int i = 2;
 
+	cout << sum(i);
 
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
 	return (0);
+}
+
+int sum(int n);
+{
+	int sum = 0;
+
+	while (fib(n) < N)
+	{
+		if (fib(n) % 2 == 0)
+		{
+
+			sum += fib(n);
+		}
+
+		n++;
+	}
+	return (sum);
+}
+
+int fib(int n);
+{
+
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	int i = 1;
+	while (i <= n)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		i++;
+	}
+
+	return (c);
 }
