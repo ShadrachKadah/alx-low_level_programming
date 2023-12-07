17f210bcf69dfbdabcec685e1c564d8afb81751a
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-12-07 20:50:01 +0000

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*
+/**
  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
  *                            list at a given position.
  * @h: A pointer to the head of the dlistint_t list.
@@ -12,35 +12,53 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp = *h, *new;
+    dlistint_t *tmp = *h, *new_node;
+
+    /* Allocate memory for the new node. */
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    /* Set the value of the new node. */
+    new_node->n = n;
 
     /* If the index is 0, insert at the beginning of the list. */
     if (idx == 0)
-        return (add_dnodeint(h, n));
+    {
+        new_node->next = *h;
+        new_node->prev = NULL;
+        if (*h != NULL)
+            (*h)->prev = new_node;
+        *h = new_node;
+        return (new_node);
+    }
 
     /* Traverse the list to reach the desired position. */
     for (; idx != 1; idx--)
     {
-        tmp = tmp->next;
         if (tmp == NULL)
+        {
+            free(new_node);
             return (NULL);
+        }
+        tmp = tmp->next;
     }
 
     /* If the desired position is at the end, insert at the end of the list. */
     if (tmp->next == NULL)
-        return (add_dnodeint_end(h, n));
+    {
+        new_node->next = NULL;
+        new_node->prev = tmp;
+        tmp->next = new_node;
+        return (new_node);
+    }
 
-    /* Allocate memory for the new node. */
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+    /* Set the links for the new node and update the adjacent nodes' links. */
+    new_node->next = tmp->next;
+    new_node->prev = tmp;
+    tmp->next->prev = new_node;
+    tmp->next = new_node;
 
-    /* Set the value of the new node and update the links. */
-    new->n = n;
-    new->prev = tmp;
-    new->next = tmp->next;
-    tmp->next->prev = new;
-    tmp->next = new;
-
-    return (new);
+    return (new_node);
 }
+
