aa9a673c5786e20e3e6432da8d360a46b2aa0339
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-12-07 20:16:42 +0000

    6. Sum list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,45 +1,47 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
-/**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Pointer to a pointer to the head of the list.
- * @idx: Index where the new node should be added.
- * @n: Value of the new node.
+/*
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- * Return: Address of the new node, or NULL if it failed.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	dlistint_t *new_node, *current = *h;
+	dlistint_t *tmp = *h, *new;
 
+    /* If the index is 0, insert at the beginning of the list. */
 	if (idx == 0)
-	return add_dnodeint(h, n); // Use add_dnodeint to insert at the beginning
+	return (add_dnodeint(h, n));
 
-	while (current != NULL && count < idx - 1)
+    /* Traverse the list to reach the desired position. */
+	for (; idx != 1; idx--)
 	{
-	current = current->next;
-	count++;
+	tmp = tmp->next;
+	if (tmp == NULL)
+		return (NULL);
 	}
 
-	if (current == NULL) // Index out of range
-	return NULL;
+    /* If the desired position is at the end, insert at the end of the list. */
+	if (tmp->next == NULL)
+	return (add_dnodeint_end(h, n));
 
-    // Create new node
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	return NULL;
+    /* Allocate memory for the new node. */
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	return (NULL);
 
-	new_node->n = n;
+    /* Set the value of the new node and update the links. */
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-	if (current->next != NULL)
-	current->next->prev = new_node;
-
-	ew_node->next = current->next;
-	new_node->prev = current;
-	current->next = new_node;
-
-	return new_node;
+	return (new);
 }
+
