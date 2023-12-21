91b414e4e98e5e58641d2e872ec7f426c6886a4c
ShadrachKadah <shadrachaudukadah1@gmail.com>
2023-12-21 11:08:26 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,27 +2,29 @@
 
 /**
  * hash_table_create - creates a hash table
- * @table_size: size of the hash table
+ * @size: size of the hash table
  *
  * Return: pointer to hash table
  */
-hash_table_t *hash_table_create(unsigned long int table_size)
+
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *table;
 
-	if (table_size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-	new_table = calloc(1, sizeof(hash_table_t));
-	if (new_table == NULL)
-	return (NULL);
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-	new_table->size = table_size;
-	new_table->array = calloc(table_size, sizeof(hash_node_t *));
-	if (new_table->array == NULL)
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
-	free(new_table);
-	return (NULL);
+		free(table);
+		return (NULL);
 	}
-	return (new_table);
+	return (table);
 }
+
